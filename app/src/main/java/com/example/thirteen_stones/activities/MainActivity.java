9e27f5b60f5fc2c77a88721c0c9ddf904289bd59
rachel613g <rachelgutmann613@gmail.com>
2020-11-09 22:35:00 -0500

    updated Java logic - as usual code was written by Professor. (end of module 7)

--- app/src/main/java/com/example/thirteen_stones/activities/MainActivity.java
+++ app/src/main/java/com/example/thirteen_stones/activities/MainActivity.java
@@ -5,89 +5,221 @@ import android.os.Bundle;
 
 import com.example.thirteen_stones.R;
 import com.example.thirteen_stones.lib.DialogUtils;
+import com.example.thirteen_stones.models.ThirteenStones;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import static com.example.thirteen_stones.lib.DialogUtils.showInfoDialog;
+import static com.example.thirteen_stones.models.ThirteenStones.getGameFromJSON;
+import static com.example.thirteen_stones.models.ThirteenStones.getJSONFromGame;
 
 public class MainActivity extends AppCompatActivity {
+    private ThirteenStones mGame;
+    private TextView mTvStatusBarCurrentPlayer, mTvStatusBarStonesRemaining;
+    private ImageView mImageViewStones;
+    private Snackbar mSnackBar;
+
+    private int[] mImages;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("GAME", getJSONFromGame(mGame));
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mGame = getGameFromJSON(savedInstanceState.getString("GAME"));
+        updateUI();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
+        setupViews();
+        setupImagesIntArray();
+        setupFAB();
+        startFirstGame();
+    }
+
+    private void setupViews() {
+        mTvStatusBarCurrentPlayer = findViewById(R.id.tv_status_current_player);
+        mTvStatusBarStonesRemaining = findViewById(R.id.tv_status_stones_remaining);
+        mSnackBar =
+                Snackbar.make(findViewById(android.R.id.content), getString(R.string.welcome),
+                        Snackbar.LENGTH_LONG);
+        mImageViewStones = findViewById(R.id.image_view_stones);
+    }
+
+    private void setupImagesIntArray() {
+        mImages = new int[]{R.drawable.stones_00, R.drawable.stones_01, R.drawable.stones_02,
+                R.drawable.stones_03, R.drawable.stones_04, R.drawable.stones_05,
+                R.drawable.stones_06, R.drawable.stones_07, R.drawable.stones_08,
+                R.drawable.stones_09, R.drawable.stones_10, R.drawable.stones_11,
+                R.drawable.stones_12, R.drawable.stones_13};
+    }
+
+    private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
+    private void setupFAB() {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showInfoDialog(MainActivity.this,
+                        getString(R.string.info_title), mGame.getRules());
             }
         });
     }
 
+    @SuppressWarnings("unused")
+    public void pick123(View view) {
+        try {
+            mGame.takeTurn(Integer.parseInt(((Button) view).getText().toString()));
+            updateUI();
+            showGameOverMessageIfGameNowOver();
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            if (e.getMessage() != null) {
+                mSnackBar.setText(e.getMessage());
+                mSnackBar.show();
+            }
+        }
+    }
+
+    private void dismissSnackBarIfShown() {
+        if (mSnackBar.isShown()) {
+            mSnackBar.dismiss();
+        }
+    }
+
+    private void showGameOverMessageIfGameNowOver() {
+        if (mGame.isGameOver()) {
+            dismissSnackBarIfShown();
+            showInfoDialog(this, getString(R.string.game_over),
+                    getString(R.string.winner_is_player_number) +
+                            mGame.getWinningPlayerNumberIfGameOver() +
+                            ". " + getString(
+                            R.string.games_played) + " " + mGame.getNumberOfGamesPlayed());
+        }
+    }
+
+    private void updateUI() {
+        dismissSnackBarIfShown();
+        mTvStatusBarCurrentPlayer.setText(
+                String.format(Locale.getDefault(), "%s: %d",
+                        getString(R.string.current_player),
+                        mGame.getCurrentPlayerNumber()));
+        mTvStatusBarStonesRemaining.setText
+                (String.format(Locale.getDefault(), "%s: %d",
+                        getString(R.string.stones_remaining_in_pile),
+                        mGame.getStonesRemaining()));
+        try {
+            mImageViewStones.setImageDrawable(
+                    ContextCompat.getDrawable(this, mImages[mGame.getStonesRemaining()]));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            mSnackBar.setText(R.string.error_msg_could_not_update_image);
+            mSnackBar.show();
+        }
+    }
+
+    private void startFirstGame() {
+        mGame = new ThirteenStones();
+        updateUI();
+    }
+
+    private void startNextNewGame() {
+        mGame.startGame();
+        updateUI();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.action_new_game:
+        switch (item.getItemId()) {
+            case R.id.action_new_game: {
                 startNextNewGame();
                 return true;
-            case R.id.action_statistics:
+            }
+            case R.id.action_statistics: {
                 showStatistics();
                 return true;
-            case R.id.action_reset_stats:
-                // mGame.resetStatistics();
+            }
+            case R.id.action_reset_stats: {
+                mGame.resetStatistics();
                 return true;
-            case R.id.action_settings:
+            }
+            case R.id.action_settings: {
                 showSettings();
                 return true;
-            case R.id.action_about:
+            }
+            case R.id.action_about: {
                 showAbout();
                 return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
-    }
 
-    private void startNextNewGame() {
     }
 
     private void showStatistics() {
+        dismissSnackBarIfShown();
         Intent intent = new Intent(getApplicationContext(), StatisticsActivity.class);
+        intent.putExtra("GAME", mGame.getJSONFromCurrentGame());
         startActivity(intent);
     }
 
-    private void showSettings() {
-    }
-
     private void showAbout() {
-        DialogUtils.showInfoDialog(this, "About 13 Stones",
-                "This is our second in-class app of the semester!");
+        dismissSnackBarIfShown();
+        showInfoDialog(MainActivity.this, "About 13 Stones",
+                "A quick two-player game; have fun!\n" +
+                        "\nAndroid game by SA.\nmintedtech@gmail.com");
     }
 
+    private void showSettings() {
+        dismissSnackBarIfShown();
+        //Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+        //startActivityForResult(intent, 1);
+    }
 
-    public void pick123(View view) {
-        //handle clicks on 1, 2 and 3
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1) {
+            applySettingsChanges();
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void applySettingsChanges() {
+        // Not Yet Implemented
     }
 }
--- app/src/main/java/com/example/thirteen_stones/activities/StatisticsActivity.java
+++ app/src/main/java/com/example/thirteen_stones/activities/StatisticsActivity.java
@@ -1,7 +1,9 @@
 package com.example.thirteen_stones.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.thirteen_stones.models.ThirteenStones;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -11,28 +13,39 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.thirteen_stones.R;
 
+import java.util.Locale;
+
 public class StatisticsActivity extends AppCompatActivity {
+
+    private TextView tvDataGamesPlayed,
+            tvDataPlayer1Wins, tvDataPlayer1WinsPercent,
+            tvDataPlayer2Wins, tvDataPlayer2WinsPercent;
+
+    private ThirteenStones mCurrentGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
-        setUpToolbar();
-        setUpFAB();
+        setupToolbar();
+        setupFAB();
+        setupViews();
+        getIncomingData();
+        processAndOutputIncomingData();
     }
 
-    private void setUpToolbar() {
+    private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        if(getSupportActionBar() != null)
+        if (getSupportActionBar() !=null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
     }
 
-    private void setUpFAB() {
+    private void setupFAB() {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,10 +55,41 @@ public class StatisticsActivity extends AppCompatActivity {
         });
     }
 
+    private void setupViews() {
+        tvDataGamesPlayed = findViewById(R.id.tv_data_games_played);
+        tvDataPlayer1Wins = findViewById(R.id.tv_data_player1_wins);
+        tvDataPlayer1WinsPercent = findViewById(R.id.tv_data_player1_win_percent);
+        tvDataPlayer2Wins = findViewById(R.id.tv_data_player2_wins);
+        tvDataPlayer2WinsPercent = findViewById(R.id.tv_data_player2_win_percent);
+    }
+
+    private void getIncomingData() {
+        Intent intent = getIntent();
+        String gameJSON = intent.getStringExtra("GAME");
+        mCurrentGame = ThirteenStones.getGameFromJSON(gameJSON);
+    }
+
+    private void processAndOutputIncomingData() {
+        final String FORMAT_STRING = "%2.1f%%", N_A = "N/A";
+        int numberOfGamesPlayed = mCurrentGame.getNumberOfGamesPlayed();
+        if (!mCurrentGame.isGameOver() && numberOfGamesPlayed > 0)
+            numberOfGamesPlayed--;
+        int p1Wins = mCurrentGame.getNumberOfWinsForPlayer(1);
+        int p2Wins = mCurrentGame.getNumberOfWinsForPlayer(2);
+        String p1WinPct = numberOfGamesPlayed  == 0 ? N_A :
+                String.format(Locale.US, FORMAT_STRING, (p1Wins/(double)numberOfGamesPlayed)*100);
+        String p2WinPct = numberOfGamesPlayed == 0 ? N_A :
+                String.format(Locale.US, FORMAT_STRING, (p2Wins/(double)numberOfGamesPlayed)*100);
+        tvDataGamesPlayed.setText(String.valueOf(numberOfGamesPlayed));     // don't forget String.valueOf()
+        tvDataPlayer1Wins.setText(String.valueOf(p1Wins));
+        tvDataPlayer2Wins.setText(String.valueOf(p2Wins));
+        tvDataPlayer1WinsPercent.setText(p1WinPct);
+        tvDataPlayer2WinsPercent.setText(p2WinPct);
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()== android.R.id.home)
-        {
+        if (item.getItemId() == android.R.id.home) {
             onBackPressed();
             return true;
         } else
