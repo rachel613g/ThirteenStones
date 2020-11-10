f3b64d2c6270df0a5b2e830b5855fc2cee30d893
rachel613g <rachelgutmann613@gmail.com>
2020-11-09 22:15:31 -0500

    updated statistics activity and view.

--- app/src/main/java/com/example/thirteen_stones/activities/StatisticsActivity.java
+++ app/src/main/java/com/example/thirteen_stones/activities/StatisticsActivity.java
@@ -37,8 +37,7 @@ public class StatisticsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onBackPressed();
             }
         });
     }
